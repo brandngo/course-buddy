1d150ff067427ffeec75f4810da50b185fdb2f49
abhqy <abhaymenon7@gmail.com>
2021-05-07 18:42:25 -0400

    Completed login system

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Auth from "./screens/Auth"
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth />
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -1,89 +1,39 @@
-import React, { Component } from 'react';
+import firebase from "firebase";
+import React from "react";
+import App from "../App";
+import Enter from "./Enter";
 
-export default class Auth extends Component {
+export default class Auth extends React.Component {
     constructor() {
         super();
         this.state = {
-            username: undefined,
-            password: undefined,
-            message: "",
-            isLogged: false
+            user: null,
+            usernameValue: "",
         };
+        this.authListener = this.authListener.bind(this);
     }
 
-    checkIfUserExistsWhileSignUp() {
-        var ref = firebase.database().ref("users");
-        ref.child(this.state.username).once("value", (snap) => {
-            if (snap.exists()) {
-                this.setState({ message: "Username already taken!" });
-            } else {
-                this.signUp();
-            }
+    authListener() {
+        var auth = firebase.auth();
+        auth.onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ user });
+            } else this.setState({ user: null });
         });
     }
 
-    signUp() {
-        var formatUsername = /[!-\/:-@[-`{-~ ]/;
-        var formatPassword = /[^ -~]/;
+    initialToggle
 
-        if ((this.state.username.length <= 6) |
-            (this.state.password.length <= 6) |
-            formatUsername.test(this.state.username) |
-            formatPassword.test(this.state.password)) {
-            this.setState({ message: "Invalid username or password!" });
-        } else {
-            var ref = firebase.database().ref("users");
-            ref.child(this.state.username).set({
-                username: this.state.username,
-                password: this.state.password,
-                isLogged: true
-            });
-        }
-    }
-
-    checkIfUserExistsWhileLogIn() {
-        var ref = firebase.database().ref("users");
-        ref.child(this.state.username).once("value", (snap) => {
-            console.log(snap.val());
-            if (snap.exists()) {
-                if (snap.val().password !== this.state.password) {
-                    this.setState({ message: "Wrong Password!" });
-                }
-                // Logging in after know that the user actually exists
-                this.logIn();
-            } else {
-                this.setState({ message: "Account does not exist!" });
-            }
-        });
-    }
-
-    logIn() {
-        this.setState({ isLogged: true });
+    componentDidMount() {
+        this.authListener();
+        if (this.state.user) {
+            this.setState({ user: this.state.user });
+        } else this.setState({ user: null });
     }
 
     render() {
-        if (!this.state.isLogged) { // Not logged in here, forms are displayed
-            return (
-                <div> // Container Div.
-                    <input placeholder="Username"
-                        type="text"
-                        onChange={(e) =>
-                            this.setState({ username: e.target.value })}></input>
-                    <br />
-                    <input placeholder="Password"
-                        type="text"
-                        onChange={(e) =>
-                            this.setState({ password: e.target.value })}></input>
-                    <button onClick={this.checkIfUserExistsWhileSignUp.bind(this)}>
-                        Sign Up!
-                </button>
-                    <button onClick={this.checkIfUserExistsWhileLogIn.bind(this)}>
-                        Log In!
-                </button>
-                </div> // Container Div.
-            )
-        } else {
-            <div>You're logged in!</div> // Logged in here
-        }
+        if (this.state.user) {
+            return <App user={this.state.user}></App>;
+        } else return <Enter />;
     }
 }
