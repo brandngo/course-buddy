3d716b0e3d387e0591bef0a51a9cdbae6c543430
Aaryaman0 <abhageri@uwaterloo.ca>
2021-05-07 23:38:56 -0400

    frontend design + requirements completed

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,101 +1,156 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { DragDropContext } from 'react-beautiful-dnd'
+//import { DragDropContext } from 'react-beautiful-dnd'
 import styled from 'styled-components'
+import Box from '@material-ui/core/Box';
+import "./Home.css"
+import { Table } from "react-bootstrap"
 
-import initialData from './initial-data'
-import Droppables from '../components/DnDComponents/Droppables.js'
+//import initialData from './initial-data'
+//import Droppables from '../components/DnDComponents/Droppables.js'
 
 const Container = styled.div`
   display:flex;
 `
 
 class Home extends React.Component {
-  state = initialData
-
-  onDragEnd = result => {
-    const { destination, source, draggableId } = result
-
-    if (!destination) {
-      return
-    }
-
-    if (
-      destination.droppableId === source.droppableId &&
-      destination.index === source.index
-    ) {
-      return
-    }
-
-    const start = this.state.columns[source.droppableId]
-    const finish = this.state.columns[destination.droppableId]
-
-    if (start === finish) {
-      const newTaskIds = Array.from(start.taskIds)
-      newTaskIds.splice(source.index, 1)
-      newTaskIds.splice(destination.index, 0, draggableId)
-
-      const newColumn = {
-        ...start,
-        taskIds: newTaskIds
-      }
-
-      const newState = {
-        ...this.state,
-        columns: {
-          ...this.state.columns,
-          [newColumn.id]: newColumn
-        }
-      }
-
-      this.setState(newState)
-      return
-    }
-
-    // Moving from one list to another
-    const startTaskIds = Array.from(start.taskIds)
-    startTaskIds.splice(source.index, 1)
-    const newStart = {
-      ...start,
-      taskIds: startTaskIds
-    }
-
-    const finishTaskIds = Array.from(finish.taskIds)
-    finishTaskIds.splice(destination.index, 0, draggableId)
-    const newFinish = {
-      ...finish,
-      taskIds: finishTaskIds
-    }
-
-    const newState = {
-      ...this.state,
-      columns: {
-        ...this.state.columns,
-        [newStart.id]: newStart,
-        [newFinish.id]: newFinish
-      }
-    }
-    this.setState(newState)
-  }
-
-  render() {
-    return (
-      <DragDropContext onDragEnd={this.onDragEnd}>
-        <Container>
-          {this.state.columnOrder.map(columnId => {
-            const column = this.state.columns[columnId]
-            const tasks = column.taskIds.map(
-              taskId => this.state.tasks[taskId]
-            )
-
-            return (
-              <Droppables key={column.id} column={column} tasks={tasks} />
-            )
-          })}
-        </Container>
-      </DragDropContext>
-    )
-  }
+render() {
+  return (
+    <div>
+    <h1>Term-Planner:</h1>
+    <div className="container" width="100vw">
+      <Box border={1} width="12.5%" height="40vh" alignItems="center">
+      <h1>1a</h1>
+      </Box>
+      <Box border={1} width="12.5%" height="40vh" alignItems="center">
+      <h1>1b</h1>
+      </Box>
+      <Box border={1} width="12.5%" height="40vh" alignItems="center">
+      <h1>2a</h1>
+      </Box>
+      <Box border={1} width="12.5%" height="40vh" alignItems="center">
+      <h1>2b</h1>
+      </Box>
+      <Box border={1} width="12.5%" height="40vh" alignItems="center">
+      <h1>3a</h1>
+      </Box>
+      <Box border={1} width="12.5%" height="40vh" alignItems="center">
+      <h1>3b</h1>
+      </Box>
+      <Box border={1} width="12.5%" height="40vh" alignItems="center">
+      <h1>4a</h1>
+      </Box>
+      <Box border={1} width="12.5%" height="40vh" alignItems="center">
+      <h1>4b</h1>
+      </Box>
+      </div>
+      <h2 style={{position: 'relative', textAlign: 'center'}}>These are the courses:</h2>
+      <div className="container2">
+      <Box border={1} width="25%" height="100vh" alignItems="center">
+      <h1>Required Courses</h1>
+      </Box>
+      <Box border={1} width="50%" height="100vh" alignItems="center">
+      <h1>Recommended Courses</h1>
+      </Box>
+      <Box border={1} width="12.5%" height="100vh" alignItems="center">
+      <h1>Electives</h1>
+      </Box>
+      <Box border={1} width="12.5%" height="100vh" alignItems="center">
+      <h1>Comms</h1>
+      </Box>
+      </div>
+      <h2>General Requirements</h2>
+      <p>1. All required Math/CS courses must be taken</p>
+      <p>2. 3 courses from the first list of recommended courses</p>
+      <p>3. 2 courses from the second list of recommended courses</p>
+      <p>4. 2 Humanities courses, 2 Social Science courses,
+          1 Pure science course, 1 Pure/Applied Science course,
+          3 courses of any type (one at 3XX level)</p>
+      <h2>Special Requirements (per course)</h2>
+      <p>1. The following courses require CS 240,241 :- 
+        CS 348, CS 349, CS 350, CS 360, CS 365, CS 370, CS 371, CS 383, CS 442, CS 449.</p>
+      <p>2. The following courses require CS 350 :- 
+        CS 343, CS 444, CS 445, CS 446, CS 447, CS 448, CS 371, CS 383, CS 450, CS 451, CS 452, 
+        CS 454, CS 456, CS 457, CS 458.</p>
+      <p>3. The following courses require CS 341 :- CS 451, CS 466, CS 482, CS 485, CS 486, CS 488</p>
+      <p>4. The following courses require CS 370/371 :- CS 475, CS 476, CS 479, CS 484</p>
+      <p>5. The following courses require CS 360/365 :- CS 462</p>
+      <h2>Suggested Course Sequence</h2>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>1A</th>
+            <th>1B</th>
+            <th>2A</th>
+            <th>2B</th>
+            <th>3A</th>
+            <th>3B</th>
+            <th>4A</th>
+            <th>4B</th>
+            </tr>
+            </thead>
+          <tbody>
+            <tr>
+            <th>1</th>
+            <th>Cs135/145</th>
+            <th>Cs136/146</th>
+            <th>Cs246</th>
+            <th>Cs240</th>
+            <th>Cs341</th>
+            <th>Cs3XX/4XX</th>
+            <th>Courses</th>
+            <th>Courses</th>
+            </tr>
+            <tr>
+            <th>2</th>
+            <th>Math135</th>
+            <th>Math136</th>
+            <th>Cs245</th>
+            <th>Cs241</th>
+            <th>Cs350</th>
+            <th>Cs3XX/4XX</th>
+            <th>To</th>
+            <th>To</th>
+            </tr>
+            <tr>
+            <th>3</th>
+            <th>Math137</th>
+            <th>Math138</th>
+            <th>Stat230</th>
+            <th>Cs251</th>
+            <th>Stat231</th>
+            <th>Non-Math</th>
+            <th>Complete</th>
+            <th>Complete</th>
+            </tr>
+            <tr>
+            <th>4</th>
+            <th>List 1 Communication</th>
+            <th>List 2 Communication</th>
+            <th>Non-Math</th>
+            <th>Math239</th>
+            <th>Non-math</th>
+            <th>Non-math</th>
+            <th>Degree</th>
+            <th>Degree</th>
+            </tr>
+            <tr>
+            <th>5</th>
+            <th>Non-math</th>
+            <th>Non-math</th>
+            <th>Non-math</th>
+            <th>Non-math</th>
+            <th>Elective</th>
+            <th>Elective</th>
+            <th>Requirements</th>
+            <th>Requirements</th>
+            </tr>
+            </tbody>
+            </Table>
+    </div>
+  )
+}
 }
 
 export default Home;
